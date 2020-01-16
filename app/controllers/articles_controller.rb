de86781620969e6cc745de9de37c6e7700a9ca7d
Molham <molhammilo@gmail.com>
2020-01-16 22:24:55 +0200

    ,making function to run before action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-
+  before_action :set_article , only: [ :edit , :update , :show , :destroy ]
   def index
    @articles = Article.all
   end
@@ -8,8 +8,7 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
-  def create
-    #render plain: params[:article].inspect    
+  def create    
     @article = Article.new(article_params)
     if @article.save
       flash[:notice] = " Your article was successfully created ! "
@@ -20,15 +19,12 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
   
   def update
-    @article = Article.find(params[:id])
     
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated"
@@ -39,7 +35,6 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
 
     flash[:notice] = "Article was deleted"
@@ -47,6 +42,10 @@ class ArticlesController < ApplicationController
   end
 
   private
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
     def article_params
       params.require(:article).permit( :title , :description )
     end
